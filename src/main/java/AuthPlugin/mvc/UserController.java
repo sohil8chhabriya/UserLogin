b2edcbca858ab40175526aa0bb19951e1d6ccb27
sohil8chhabriya <sohil8chhabriya@gmail.com>
2015-04-15 11:17:09 +0530

    adding userlogin project

---
+++ src/main/java/AuthPlugin/mvc/UserController.java
@@ -0,0 +1,13 @@
+package AuthPlugin.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by sohil chhabriya on 15-Apr-15.
+ */
+
+@Controller
+@RequestMapping(value="/")
+public class UserController {
+}
